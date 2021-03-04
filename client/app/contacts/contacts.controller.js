1294a31d4a6266a33ce543d1584e958fc0a2aff4
Heloisa Hungaro <heloisa.hungaro@gmail.com>
2021-03-04 15:24:19 -0300

    Add contacts and users controllers/views

---
+++ client/app/contacts/contacts.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular
+.module('agenda')
+  .controller('Contacts', function($scope) {
+
+  });
---
+++ client/app/users/users.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular
+.module('agenda')
+  .controller('Users', function($scope) {
+
+  });
