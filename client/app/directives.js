3df3cf8582401a6dda5c77cd0d095896900d1a44
Heloisa Hungaro <heloisa.hungaro@gmail.com>
2021-03-04 15:24:33 -0300

    Add directive to autofocus input

---
+++ client/app/directives.js
@@ -0,0 +1,12 @@
+angular
+  .module('agenda')
+    .directive('autoFocus', function($timeout) {
+      return {
+          restrict: 'AC',
+          link: function(_scope, _element) {
+              $timeout(function(){
+                  _element[0].focus();
+              }, 0);
+          }
+      };
+    });
